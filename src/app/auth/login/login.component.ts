51ae0fc8ffecc262e9c9a715a2e569e4fc8a9cef
YJP-0104 <patel.yashjat@northeastern.edu>
2024-11-30 12:44:11 -0500

    first commit

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
-
+import { trigger, transition, style, animate } from '@angular/animations';
 import { SharedModule } from '../../shared/shared.module';
 import { AuthService } from '../../auth.service';
 import { Router } from '@angular/router';
@@ -12,7 +12,19 @@ import { Router } from '@angular/router';
 		SharedModule
 	],
 	templateUrl: './login.component.html',
-	styleUrl: './login.component.css'
+	styleUrl: './login.component.css',
+
+	animations: [
+		trigger('fadeInOut', [
+		  transition(':enter', [
+			style({ opacity: 0, transform: 'translateY(-10px)' }),
+			animate('200ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+		  ]),
+		  transition(':leave', [
+			animate('200ms ease-in', style({ opacity: 0, transform: 'translateY(-10px)' }))
+		  ])
+		])
+	  ]
 })
 export class LoginComponent {
 	hide = true;
